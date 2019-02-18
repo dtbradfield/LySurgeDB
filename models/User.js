02efd1752e9ea641708745e0b9cfeefd9ab1e3ed
DonTheChimera <donaldtbradfield@gmail.com>
2019-02-18 11:47:23 -0600

    JWT generation for user

--- models/User.js
+++ models/User.js
@@ -1,5 +1,8 @@
 let mongoose = require('mongoose');
 let uniqueValidator = require('mongoose-unique-validator');
+let crypto = require('crypto');
+let jwt = require('jsonwebtoken');
+let secret = require('../config').secret;
 
 let UserSchema = new mongoose.Schema({
     username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
@@ -11,5 +14,37 @@ let UserSchema = new mongoose.Schema({
 }, {timestamps: true});
 
 UserSchema.plugin(uniqueValidator, {message: ' is already taken'});
+
+UserSchema.methods.setPassword = (password) => {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+}
+
+UserSchema.methods.validPassword = (password) => {
+    let hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, 'sha512').toString('hex');
+    return this.hash === hash;
+}
+
+UserSchema.methods.generateJWT = () => {
+    let today = new Date();
+    let exp = new Date(today);
+    exp.setDate(today.getDate() + 60);
+
+    return jwt.sign({
+        id: this._id,
+        username: this.username,
+        exp: parseInt(exp.getTime() / 1000),
+    }, secret);
+};
+
+UserSchema.methods.toAuthJSON = () => {
+    return {
+        usrname: this.username,
+        email: this.email,
+        token: this.generateJWT(),
+        bio: this.bio,
+        image: this.image
+    };
+};
 
 mongoose.model('User', UserSchema);
