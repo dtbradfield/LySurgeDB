3125946a36158a0b4fd56d47650f615b0960ac53
DonTheChimera <donaldtbradfield@gmail.com>
2019-02-18 11:34:51 -0600

    added validation to user model

---
+++ models/User.js
@@ -0,0 +1,15 @@
+let mongoose = require('mongoose');
+let uniqueValidator = require('mongoose-unique-validator');
+
+let UserSchema = new mongoose.Schema({
+    username: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/^[a-zA-Z0-9]+$/, 'is invalid'], index: true},
+    email: {type: String, lowercase: true, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
+    bio: String,
+    image: String,
+    hash: String,
+    salt: String
+}, {timestamps: true});
+
+UserSchema.plugin(uniqueValidator, {message: ' is already taken'});
+
+mongoose.model('User', UserSchema);
